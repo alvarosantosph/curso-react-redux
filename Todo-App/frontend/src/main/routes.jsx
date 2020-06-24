9986b986048c75c2c7fc9edb3f02e85ed64f006b
Alvaro Santos <siruz_slipknot@hotmail.com>
2020-06-24 13:43:39 -0300

    TodoApp - ToDoList

--- Todo-App/frontend/src/main/routes.jsx
+++ Todo-App/frontend/src/main/routes.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
-
 import Todo from '../todo/todo'
 import About from '../about/about'
-import about from '../about/about'
 
 export default props => (
     <Router history={hashHistory}>
         <Route path='/todos' component={Todo} />
-        <Route path='/about' component={about} />
-        <Route from='*' to='/todos' />
+        <Route path='/about' component={About} />
+        <Redirect from='*' to='/todos' />
     </Router>
 )
